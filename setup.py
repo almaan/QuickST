ece076f188ffea904c9ed95ab74fcbcbba7d0ede
almaan <almaan@kth.se>
2019-06-17 07:57:03 +0200

    dataloading

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+import os
+
+setup(name='QuickST',
+            version='0.1',
+            description='For easy handling of ST-data',
+            url='http://github.com/almaan/QuickST',
+            author='Alma Andersson',
+            author_email='alma.andersson@scilifelab.se',
+            license='MIT',
+            packages=['QuickST'],
+            install_requires=[
+                            'numpy',
+                            'pandas',
+                            'scipy',
+                            'logging',
+                            'argparse',
+                            'matplotlib',
+                            'sklearn',
+                            'umap-learn',
+                      ],
+            zip_safe=False)
+
+
+
