c05fd9524074ede2bf5c497be9823bf9c88cb6a7
almaan <almaan@kth.se>
2019-06-17 10:01:43 +0200

    utils

--- QuickST/data/utils.py
+++ QuickST/data/utils.py
@@ -64,4 +64,61 @@ def split_samples(joint_matrix : pd.DataFrame,
         s_start = s_end
         
     return samples
+
+def control_lists(list1 : list,
+                  list2 : list) -> bool:
+    """
+    Control if the sorting of two lists
+    are optimal in terms of similarity.
+    Returns true if lists seems to be 
+    optimally sorted and false otherwise.
+    
+    
+    Arguments
+    ---------
+        list1 : list
+            first list to be compared
+        list2 : list
+            second list to be compared
+    
+    Returns
+    ------
+    
+    """
+    
+    def maxham(s1,s2):
+        abscore = 0.0
+        if len(s1) > len(s2):
+            major,minor = s1,s2
+        else:
+            major, minor = s2,s1
+        
+        w = len(minor)
+        for pos in range(len(major) - w +1):
+            tscore = hamming(major[pos:pos + w],minor)
+            if tscore > abscore:
+                abscore = tscore
+                
+        return abscore
+    
+    def hamming(x,y):
+        hd = 0.0
+        for (xs,ys) in zip(x,y):
+            if xs == ys:
+                hd += 1
+                
+        return hd
+
+    if not len(list1) == len(list2):
+        return False
+    else:
+        smat = np.zeros((len(list1),len(list2)))
+        for l1 in range(len(list1)):
+            for l2 in range(len(list2)):
+                smat[l1,l2] = maxham(list1[l1],list2[l2])
+        
+        diag = np.diag(smat)
+        mx = np.max(smat, axis = 1)
+        
+        return (mx == diag).all()
     
